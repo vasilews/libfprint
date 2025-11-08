c7ab8e992c4a32a0e73898e103014dbef5b83277
With Mask <1167710-real_with_mask@users.noreply.gitlab.freedesktop.org>
2025-11-08 10:30:27 +0000

    feat: Add support for Synaptics 06cb:0169

--- libfprint/drivers/synaptics/synaptics.c
+++ libfprint/drivers/synaptics/synaptics.c
@@ -53,6 +53,7 @@ static const FpIdEntry id_table[] = {
   { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x0129,  },
   { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x015F,  },
   { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x0168,  },
+  { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x0169,  },
   { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x016C,  },
   { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x0173,  },
   { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x0174,  },
