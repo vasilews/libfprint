83367979a13899da12dedb303dd420b5a59a4123
Aris Lin <aris.lin@synaptics.com>
2025-11-21 17:37:57 +0800

    synaptics: add new PID 0x1A0

--- libfprint/drivers/synaptics/synaptics.c
+++ libfprint/drivers/synaptics/synaptics.c
@@ -59,6 +59,7 @@ static const FpIdEntry id_table[] = {
   { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x0174,  },
   { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x019D,  },
   { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x019F,  },
+  { .vid = SYNAPTICS_VENDOR_ID,  .pid = 0x01A0,  },
   { .vid = 0,  .pid = 0,  .driver_data = 0 },   /* terminating entry */
 };
 
